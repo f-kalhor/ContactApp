d11e055151dfc39e14ce182a6017b23de74ad8d9
unknown <mohsenkalhor73@gmail.com>
2025-04-01 13:11:42 +0430

    add button for adding contacts

--- src/Cantacts.jsx
+++ src/Cantacts.jsx
@@ -4,29 +4,65 @@ import { useState } from "react";
 
 function Cantacts() {
   const [contact, setContact] = useState({
-    name:"",
+    name: "",
     lastName: "",
     eamil: "",
     phone: "",
   });
-  const loginHandler=()=>{
+  const [contacts, setContacts] = useState([])
+
+  const addHandler = () => {
+    setContacts((contacts)=>([ ...contacts, contact]))
     console.log(contact);
-    
-  }
+    console.log(contacts);
+    setContact({
+      name: "",
+      lastName: "",
+      eamil: "",
+      phone: "",
+    }) 
+  };
   const changHandler = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setContact((contact)=> ({...contact , [name]:value}))
-  }
-  return (<>
-    <div>
-      <input type="text" placeholder="Name" onChange={changHandler} name="name" value={contact.name}/>
-      <input type="text" placeholder="Last Name" onChange={changHandler} name="lastName"value={contact.lastName}/>
-      <input type="text" placeholder="Email" onChange={changHandler} name="eamil"value={contact.eamil}/>
-      <input type="text" placeholder="Phone" onChange={changHandler} name="phone"value={contact.phone}/>
-      <button onClick={loginHandler}> login</button>
-    </div>
-    </>);
+    setContact((contact) => ({ ...contact, [name]: value }));
+    console.log(name, value);
+  };
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={changHandler}
+          name="name"
+          value={contact.name}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          onChange={changHandler}
+          name="lastName"
+          value={contact.lastName}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          onChange={changHandler}
+          name="eamil"
+          value={contact.eamil}
+        />
+        <input
+          type="text"
+          placeholder="Phone"
+          onChange={changHandler}
+          name="phone"
+          value={contact.phone}
+        />
+        <button onClick={addHandler}>AddHandler</button>
+      </div>
+    </>
+  );
 }
 
 export default Cantacts;
